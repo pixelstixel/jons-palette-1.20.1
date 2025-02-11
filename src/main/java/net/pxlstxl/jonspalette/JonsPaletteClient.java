b3cd9b714ceb26366e5a83e5a7b4bb812ffba1a7
jon <pixelstixelpixel@gmail.com>
2025-02-11 01:40:00 -0800

    Initial commit

---
+++ src/main/java/net/pxlstxl/jonspalette/JonsPaletteClient.java
@@ -0,0 +1,10 @@
+package net.pxlstxl.jonspalette;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class JonsPaletteClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
